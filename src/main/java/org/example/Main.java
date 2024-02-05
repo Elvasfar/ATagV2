1c56f58e85bbf869a574312c446da634c717d1b2
erlm <erlm@kea.dk>
2024-02-05 13:25:00 +0100

    changed to vers17

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,14 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-       Kod et loop der kører 20 gange
-            i hver iteration, tilføj et HTag og et PTag til liste. og det er en ArrayList.class
+       //Kod et loop der kører 20 gange
+       //     i hver iteration, tilføj et HTag og et PTag til liste. og det er en ArrayList.class
+
+        List<Tag> tags = new ArrayList<>();
+        for (int i=1; i<=20; i++) {
+            tags.add(new HTag(1, "Hej +" + i));
+        }
+
 
 
 
